101b9a7ea2d7aaccd0ccfb2ba855f8cd55dcc34c
Svastikkka <ManshuSharma143Repo@gmail.com>
2020-09-20 22:46:37 +0530

    add pandas for creating csv

--- webscrapper.py
+++ webscrapper.py
@@ -1,4 +1,4 @@
-#features is our list
+import pandas as pd
 class LinkedListNode:
     def __init__(self,name,price=None,review=None,takingSaftyMeasure=None,pic=None,features=None):
         self.name=name
@@ -28,10 +28,10 @@ def LinkedList():
     # k review
     # l takingSaftyMeasure
     # m pic
-    #
-    
+    # n features
+
     for i,j,k,l,m in zip(hotelName,price,review,takingSaftyMeasure,pic):
-        NewNode=LinkedListNode(i.get_attribute("innerHTML"),j.get_attribute("innerHTML")[7:],k.get_attribute("innerHTML")[0:-7],l.get_attribute("innerHTML"),m.get_attribute('src'))
+        NewNode=LinkedListNode(i.get_attribute("innerHTML")[5:],j.get_attribute("innerHTML")[7:],k.get_attribute("innerHTML")[0:-7],l.get_attribute("innerHTML"),m.get_attribute('src'))
         if head is None:
             head=NewNode
             tail=NewNode
@@ -39,21 +39,34 @@ def LinkedList():
             tail.next=NewNode
             tail=NewNode
     return head
+
+hotel=[]
+price=[]
+review=[]
+takingSaftyMeasure=[]
+pic=[]
+
+#Optionally creted to see our output on cli
 def printLL(head):
     while head is not None:
+        hotel.append(head.name)
+        price.append(head.price)
+        review.append(head.review)
+        takingSaftyMeasure.append(head.takingSaftyMeasure)
+        pic.append(head.pic)
+
+
         print(head.name,head.price,head.review,head.takingSaftyMeasure,head.pic)
         head=head.next
     print()
 
-
-
-
-
-
-
 #features=driver.find_elements_by_class_name("text")
 printLL(LinkedList())
 
+dict = {'hotel': hotel, 'price': price, 'review': review,'takingSaftyMeasure': takingSaftyMeasure, 'pic': pic}
+df = pd.DataFrame(dict)
 
+# saving the dataframe
+df.to_csv('file1.csv')
 
 driver.close()
